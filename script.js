7c5e2f09351871eb2c2904bbae75b660f4bde099
Rômulo Mesquita <68628269+Romuloom@users.noreply.github.com>
2020-11-27 18:56:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        //window.alert('[ERRO] Falatm dados!')
+        res.innerHTML('Impossível contar!')
+    } else {
+        res.innerHTML = 'contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window,alert('Passo Invalido! considerando passo 1')
+            p = 1
+        }
+        if (i < f) {
+            // CONTANGEM CRECENTE
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{27A1}`
+            }
+        } else {
+            // CONTAGEM DECRECENTE 
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{27A1}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
